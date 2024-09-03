e63da075c3ac850e4a6b2cc989e20faf9cb7d114
Calvin0411 <ramjeeawoncalvin@gmail.com>
2024-09-03 20:09:49 +0200

    Carrousel Fonctionnel
    J'ai réussi à intégrer ma carrousel, il me manque plus que les détails de ma page et son style css provisoire.

--- src/components/jsx/ApartmentDetail.jsx
+++ src/components/jsx/ApartmentDetail.jsx
@@ -1,38 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import cardsData from '../../data/cardsData'; 
+import cardsData from '../../data/cardsData';
+import leftArrow from '../../assets/images/vector_left.png';
+import rightArrow from '../../assets/images/vector_right.png';
 
 function ApartmentDetail() {
   const { id } = useParams();
   const apartment = cardsData.find((item) => item.id === id);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   if (!apartment) {
     return <h2>Appartement non trouvé</h2>;
   }
 
+  const handlePrev = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? apartment.pictures.length - 1 : prevIndex - 1
+    );
+  };
 
-//test
+  const handleNext = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === apartment.pictures.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  // Fonction pour générer les étoiles
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <i
+          key={i}
+          className={`fa-solid fa-star ${i <= apartment.rating ? 'filled-star' : 'empty-star'}`}
+        ></i>
+      );
+    }
+    return stars;
+  };
 
   return (
     <div className="apartment-detail">
-      <img src={apartment.cover} alt={apartment.title} className="apartment-image" />
-      <h1>{apartment.title}</h1>
-      <p>{apartment.description}</p>
-      <h3>Localisation: {apartment.location}</h3>
-      <h3>Équipements:</h3>
-      <ul>
-        {apartment.equipments.map((equipment, index) => (
-          <li key={index}>{equipment}</li>
-        ))}
-      </ul>
-      <h3>Tags:</h3>
-      <div>
-        {apartment.tags.map((tag, index) => (
-          <span key={index} className="tag">{tag}</span>
-        ))}
+      <div className="carousel">
+        <button className="carousel-button left" onClick={handlePrev}>
+          <img src={leftArrow} alt="Précédent" />
+        </button>
+
+        <img
+          src={apartment.pictures[currentImageIndex]}
+          alt={`Diapositive ${currentImageIndex + 1}`}
+          className="carousel-image"
+        />
+
+        <button className="carousel-button right" onClick={handleNext}>
+          <img src={rightArrow} alt="Suivant" />
+        </button>
+
+        <div className="carousel-indicator">
+          {currentImageIndex + 1}/{apartment.pictures.length}
+        </div>
       </div>
+
+      <h1>{apartment.title}</h1>
+
+      
     </div>
   );
 }
 
 export default ApartmentDetail;
+
