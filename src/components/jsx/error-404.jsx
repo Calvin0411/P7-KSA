5ca8bce8ca4c1f6dfa490e7955e9bcc8e34b96d1
Calvin0411 <ramjeeawoncalvin@gmail.com>
2024-09-11 13:12:27 +0200

    Style et responsive de ma page erreur terminé
    J'ai terminé de coder la page erreur et j'ai finis de lui apporter du style version responsive aussi

--- src/components/jsx/error-404.jsx
+++ src/components/jsx/error-404.jsx
@@ -5,8 +5,8 @@ const Error404 = () => {
   return (
     <div className="error-container">
       <h1>404</h1>
-      <p>La page que vous recherchez n'existe pas.</p>
-      <Link to="/">Retour à la page d'accueil</Link>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   );
 };
